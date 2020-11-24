fba912512b485353cad2103a0a5bbe266450b9ca
RuchithaBG <ruchithabg7@gmail.com>
2020-11-24 23:03:59 +0530

    Adding introduction.

---
+++ MainEWS.py
@@ -0,0 +1,8 @@
+# When a degrading system starts to approach its doomsday, it acts "funny."
+# The goal of this code is to detect this "funny" signal, if present, in
+# biological population (drosophila).
+
+# Some technical stuff:
+# "funny" signal --> Early Warning Signal
+# reduction in larvae food --> degrading environment
+ 
